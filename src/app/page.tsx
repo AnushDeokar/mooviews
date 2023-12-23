60b62a0719cbf4f17f5234e459bd8ab5799b4a85
AnushDeokar <deokaranush@gmail.com>
2023-12-23 21:02:24 +0530

    Image slider added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-'use client';
-import Image from 'next/image';
-import { motion } from 'framer-motion';
-
-const variants = {
-  visible: (custom: any) => ({
-    opacity: 1,
-    transition: { delay: custom * 0.2 },
-  }),
-};
+import { ImageSlider } from '@/components/image-slider';
 
 export default function Home() {
   return (
@@ -24,49 +15,18 @@ export default function Home() {
           </div>
         </nav>
 
-        <div className='my-10 grid w-full  grid-cols-1 font-semibold md:grid-cols-[.5fr_.6fr]'>
-          <div
-            className='text-3xl md:text-5xl lg:text-6xl'
-            style={{ lineHeight: '65px' }}
-          >
+        <div className='md:grid-flow-col-reverse my-10 grid  w-full grid-cols-1 font-semibold md:grid-cols-[.5fr_.6fr]'>
+          <div className='text-3xl md:text-5xl md:leading-10 lg:text-6xl'>
             Movie Rating Application built using{' '}
             <span className='text-red-600'>NextJs</span>
           </div>
-          <div className='flex h-full w-full gap-4'>
-            {/* <motion.div
-            className='h-[200px] w-[200px] bg-red-600'
-            animate={{ transform: "translateX(100px)" }}            
-          >
-              What
-          </motion.div> */}
-            {/* <motion.div custom={0} className='h-[200px] w-[200px] bg-red-600' variants={variants} animate={{ transform: "translateX(100px)" }}/> */}
-            {/* <motion.div custom={1} className='h-[200px] w-[200px] bg-red-600'  
-            animate={{ transform: "translateX(100px)", scale: [1, 2] }}
-            transition={{
-              duration: 1,
-              ease: "easeInOut",
-              times: [0, 1],
-              repeat: Infinity,
-              repeatDelay: 1
-            }}
-            /> */}
+          <div className='items relative hidden items-center justify-center pt-24 md:mt-0 md:flex md:pt-0'>
+            <ImageSlider height={280} width={400} />
+            {/* <ExampleSecond/> */}
+          </div>
 
-            <motion.div
-              className='h-[200px] w-[200px] bg-red-600'
-              animate={{
-                scale: [1, 2, 1],
-                x: [0, 50, 100],
-                // transform: ["translateX(20px)", "translateX(20px)", "translateX(20px)", "translateX(20px)", "translateX(20px)"]
-              }}
-              transition={{
-                duration: 4,
-                ease: 'easeInOut',
-                times: [1, 1, 1],
-                repeat: Infinity,
-                repeatDelay: 3,
-              }}
-            />
-            {/* <motion.div custom={2} className='h-[200px] w-[200px] bg-red-600' variants={variants} animate={{ transform: "translateX(100px)" }}/> */}
+          <div className='items relative flex items-center justify-center pt-24 md:mt-0 md:hidden md:pt-0'>
+            <ImageSlider height={150} width={200} />
           </div>
         </div>
       </div>
---
+++ src/components/image-slider.tsx
@@ -0,0 +1,120 @@
+'use client';
+import { motion, AnimatePresence, Variant, Variants } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import { Page } from 'framer';
+import { transform } from 'framer-motion';
+
+const images = [
+  "url('image2.jpg')",
+  "url('image3.jpg')",
+  "url('image4.jpg')",
+  "url('image5.jpg')",
+  "url('image6.jpg')",
+];
+
+const pages = [1, 2, 3, 4, 5];
+
+const indicatorSize = 10;
+const indicatorPadding = 10;
+const indicatorWidth = images.length * indicatorSize;
+const indicatorPaddingTotal = (images.length - 1) * indicatorPadding;
+const indicatorWidthTotal = indicatorWidth + indicatorPaddingTotal;
+console.log(indicatorWidthTotal);
+// const indicatorWidthTotal = 400;
+const indicatorAlpha = 0.3;
+
+export function ImageSlider({
+  width,
+  height,
+}: {
+  width: number;
+  height: number;
+}) {
+  const [current, setCurrent] = useState(0);
+  const [autoplay, setAutoplay] = useState(true);
+  useEffect(() => {
+    let interval: any;
+    let direction = 1; // 1 for increasing, -1 for decreasing
+
+    if (autoplay) {
+      // Set interval to increase and decrease currentIndex every 2 seconds
+      interval = setInterval(() => {
+        setCurrent((prev) => {
+          let nextIndex = prev + direction;
+
+          if (nextIndex < 0) {
+            // If going below 0, change direction to increasing
+            nextIndex = 1;
+            direction = 1;
+          } else if (nextIndex >= images.length) {
+            // If reaching or exceeding the length, change direction to decreasing
+            nextIndex = images.length - 2;
+            direction = -1;
+          }
+
+          return nextIndex;
+        });
+      }, 2000);
+    }
+
+    // Cleanup interval on component unmount or when autoplay is turned off
+    return () => clearInterval(interval);
+  }, [autoplay, images.length]);
+  return (
+    <>
+      <Page
+        width={width}
+        height={height}
+        radius={10}
+        currentPage={current}
+        onChangePage={(current, _) => setCurrent(current)}
+        className='absolute'
+      >
+        {images.map((image, index) => {
+          return (
+            <div
+              // className='mt-20'
+              style={{
+                width: width,
+                height: height,
+                borderRadius: 10,
+                backgroundColor: 'red',
+                backgroundImage: image, // Use image directly as the URL
+                backgroundSize: 'cover', // Optional: Adjust to fit the container
+                backgroundPosition: 'center', // Optional: Center the image
+              }}
+              key={index}
+            />
+          );
+        })}
+      </Page>
+
+      {images.map((_, index) => {
+        return (
+          <motion.div
+            className='mt-10 md:mt-0'
+            style={{
+              width: indicatorSize,
+              height: indicatorSize,
+              borderRadius: 30,
+              backgroundColor: '#fff',
+              position: 'absolute',
+              top: 'calc(50% + 100px)',
+              left: `calc(50% + ${index} * ${
+                indicatorSize + indicatorPadding
+              }px)`,
+              x: -indicatorWidthTotal / 2,
+              opacity: indicatorAlpha,
+              cursor: 'pointer',
+            }}
+            animate={{
+              opacity: current === index - 1 ? 1 : indicatorAlpha,
+            }}
+            onTap={() => setCurrent(index - 1)}
+            key={index}
+          />
+        );
+      })}
+    </>
+  );
+}
